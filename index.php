3a5c4a5b00c6ae7fbc5abff97c343e4c6d7a2e55
Thibaut Mosteau <thibaut.mosteau@lilo.org>
2024-08-27 11:11:13 +0200

    data

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
+<?php
+const ERROR_REQUIRED = 'Veuillez renseigner une todo';
+const ERROR_TOO_SHORT = 'Veuillez entrer au moins 5 caractères';
+$filename = __DIR__ . "/data/todos.json";
+$error = '';
+$todos = [];
+
+if (file_exists($filename)) {
+    $data = file_get_contents($filename);
+    $todos = json_decode($data, true) ?? [];
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $_POST = filter_input_array(INPUT_POST, [
+        "todo" => [
+            "filter" => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+            "flags" => FILTER_FLAG_NO_ENCODE_QUOTES | FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_BACKTICK
+        ]
+    ]);
+    $todo = $_POST['todo'] ?? '';
+
+    if (!$todo) {
+        $error = ERROR_REQUIRED;
+    } else if (mb_strlen($todo) < 5) {
+        $error = ERROR_TOO_SHORT;
+    }
+
+    if (!$error) {
+        $todos = [...$todos, [
+            'name' => $todo,
+            'done' => false,
+            'id' => time()
+        ]];
+        file_put_contents($filename, json_encode($todos, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+    }
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -12,11 +51,14 @@
         <div class="content">
             <div class="todo-container">
                 <h1>Ma Todo</h1>
-                <?php require_once 'includes/index.php'; ?>
-                <form action="" method="">
-                    <input type="text" name="todo" id="name">
-                    <div class="todo-form"></div>
-                    <div class="todo-list"></div>
+                <form class="todo-form" action="/" method="post">
+                    <input name="todo" type="text">
+                    <button class="btn btn-primary">Ajouter</button>
+                </form>
+                <?php if ($error) : ?>
+                    <p class="text-danger"><?= $error ?></p>
+                <?php endif; ?>
+                <div class="todo-list"></div>
             </div>
         </div>
         <?php require_once 'includes/footer.php'; ?>
